1af11b8b939479e76a31b348cc6bb2c2085e36fb
Souvik Biswas <sbis1999@gmail.com>
2021-03-05 01:35:04 +0530

    Add sign in screen UI

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_samples/res/custom_colors.dart';
+import 'package:flutterfire_samples/widgets/sign_in_form.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.firebaseNavy,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/firebase_logo.png',
+                      height: 160,
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      'FlutterFire',
+                      style: TextStyle(
+                        color: CustomColors.firebaseYellow,
+                        fontSize: 40,
+                      ),
+                    ),
+                    Text(
+                      'Authentication',
+                      style: TextStyle(
+                        color: CustomColors.firebaseOrange,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SignInForm(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
