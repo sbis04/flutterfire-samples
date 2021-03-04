ec0a0081237b7a001ce3f7278f5d48d53361cb82
Souvik Biswas <sbis1999@gmail.com>
2021-03-05 01:34:55 +0530

    Add custom colors

---
+++ lib/res/custom_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color firebaseNavy = Color(0xFF2C384A);
+  static final Color firebaseOrange = Color(0xFFF57C00);
+  static final Color firebaseAmber = Color(0xFFFFA000);
+  static final Color firebaseYellow = Color(0xFFFFCA28);
+  static final Color firebaseGrey = Color(0xFFECEFF1);
+  static final Color googleBackground = Color(0xFF4285F4);
+}
