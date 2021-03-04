3eb15dedfef84643c529f625fbd32867286c7f70
Souvik Biswas <sbis1999@gmail.com>
2021-03-05 01:35:20 +0530

    Add email & password auth logic

---
+++ lib/utils/validator.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+
+class Validator {
+  static String validateEmail({@required String email}) {
+    RegExp emailRegExp = RegExp(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+
+    if (email.isEmpty) {
+      return 'Email can\'t be empty';
+    } else if (!emailRegExp.hasMatch(email)) {
+      return 'Enter a correct email';
+    }
+
+    return null;
+  }
+
+  static String validatePassword({@required String password}) {
+    if (password.isEmpty) {
+      return 'Password can\'t be empty';
+    } else if (password.length < 5) {
+      return 'Enter a password with length greater than 6';
+    }
+
+    return null;
+  }
+}
