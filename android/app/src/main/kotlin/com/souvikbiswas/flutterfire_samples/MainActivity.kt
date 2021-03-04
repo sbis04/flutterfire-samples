f55dd7a49e9b24bcc8dccf8b343d9dde18c46ecb
Souvik Biswas <sbis1999@gmail.com>
2021-03-05 01:34:28 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/souvikbiswas/flutterfire_samples/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.souvikbiswas.flutterfire_samples
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_samples/screens/sign_in_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FlutterFire Samples',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        brightness: Brightness.dark,
+      ),
+      home: SignInScreen(),
+    );
+  }
+}
