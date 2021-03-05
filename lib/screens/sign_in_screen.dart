bd0e244c6b6633c7007ff05e6812c0f985fea9d7
Souvik Biswas <sbis1999@gmail.com>
2021-03-05 12:04:02 +0530

    Add gesture detector to unfocus from fields when tapped outside

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -8,50 +8,62 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: CustomColors.firebaseNavy,
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.only(
-            left: 16.0,
-            right: 16.0,
-            bottom: 20.0,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              Row(),
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'assets/firebase_logo.png',
-                      height: 160,
-                    ),
-                    SizedBox(height: 20),
-                    Text(
-                      'FlutterFire',
-                      style: TextStyle(
-                        color: CustomColors.firebaseYellow,
-                        fontSize: 40,
+    return GestureDetector(
+      onTap: () {
+        _emailFocusNode.unfocus();
+        _passwordFocusNode.unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: CustomColors.firebaseNavy,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16.0,
+              right: 16.0,
+              bottom: 20.0,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Row(),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'assets/firebase_logo.png',
+                        height: 160,
                       ),
-                    ),
-                    Text(
-                      'Authentication',
-                      style: TextStyle(
-                        color: CustomColors.firebaseOrange,
-                        fontSize: 40,
+                      SizedBox(height: 20),
+                      Text(
+                        'FlutterFire',
+                        style: TextStyle(
+                          color: CustomColors.firebaseYellow,
+                          fontSize: 40,
+                        ),
                       ),
-                    ),
-                  ],
+                      Text(
+                        'Authentication',
+                        style: TextStyle(
+                          color: CustomColors.firebaseOrange,
+                          fontSize: 40,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SignInForm(),
-            ],
+                SignInForm(
+                  emailFocusNode: _emailFocusNode,
+                  passwordFocusNode: _passwordFocusNode,
+                ),
+              ],
+            ),
           ),
         ),
       ),
