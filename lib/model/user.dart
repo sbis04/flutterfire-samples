a10bd3662ae2c4e6be5be5d8ef1e4b6d944c449d
Souvik Biswas <sbis1999@gmail.com>
2021-03-05 12:03:16 +0530

    Update user model to use null safety

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class User {
-  String uid;
-  String name;
-  String email;
+  String? uid;
+  String? name;
+  String? email;
 
   User({
-    @required this.uid,
-    @required this.name,
-    @required this.email,
+    required this.uid,
+    required this.name,
+    required this.email,
   });
 
   User.fromJson(Map<String, dynamic> json) {
