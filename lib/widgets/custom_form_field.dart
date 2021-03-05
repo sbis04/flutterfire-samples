59a7ef6b1c351352024cb84d2765b82eaf29b6c2
Souvik Biswas <sbis1999@gmail.com>
2021-03-05 12:44:18 +0530

    Add some more attributes to the form field

--- lib/widgets/custom_form_field.dart
+++ lib/widgets/custom_form_field.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutterfire_samples/res/custom_colors.dart';
-import 'package:flutterfire_samples/utils/validator.dart';
 
 class CustomFormField extends StatelessWidget {
   const CustomFormField({
     Key? key,
     required TextEditingController emailController,
     required FocusNode emailFocusNode,
+    required TextInputType keyboardType,
     required String label,
     required String hint,
-  })   : _emailController = emailController,
+    required Function(String value) validator,
+    this.isObscure = false,
+  })  : _emailController = emailController,
         _emailFocusNode = emailFocusNode,
+        _keyboardtype = keyboardType,
         _label = label,
         _hint = hint,
+        _validator = validator,
         super(key: key);
 
   final TextEditingController _emailController;
   final FocusNode _emailFocusNode;
+  final TextInputType _keyboardtype;
   final String _label;
   final String _hint;
+  final bool isObscure;
+  final Function(String) _validator;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: _emailController,
       focusNode: _emailFocusNode,
-      validator: (value) => Validator.validateEmail(email: value!),
+      keyboardType: _keyboardtype,
+      obscureText: isObscure,
+      cursorColor: CustomColors.firebaseYellow,
+      validator: (value) => _validator(value!),
       decoration: InputDecoration(
         labelText: _label,
         labelStyle: TextStyle(color: CustomColors.firebaseYellow),
@@ -33,18 +43,35 @@ class CustomFormField extends StatelessWidget {
         hintStyle: TextStyle(
           color: CustomColors.firebaseGrey.withOpacity(0.5),
         ),
-        fillColor: Colors.white,
+        errorStyle: TextStyle(
+          color: Colors.redAccent,
+          fontWeight: FontWeight.bold,
+        ),
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8.0),
           borderSide: BorderSide(
             color: CustomColors.firebaseAmber,
+            width: 2,
           ),
         ),
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8.0),
           borderSide: BorderSide(
             color: CustomColors.firebaseGrey.withOpacity(0.5),
-            // width: 1.0,
+          ),
+        ),
+        errorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(
+            color: Colors.redAccent,
+            width: 2,
+          ),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.0),
+          borderSide: BorderSide(
+            color: Colors.redAccent,
+            width: 2,
           ),
         ),
       ),
