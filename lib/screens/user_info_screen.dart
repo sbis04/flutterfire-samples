f2de6c6beb4a7322532cd9eb7ca993688ee793af
Souvik Biswas <sbis1999@gmail.com>
2021-03-09 16:31:40 +0530

    Update user info screen

--- lib/screens/user_info_screen.dart
+++ lib/screens/user_info_screen.dart
@@ -17,10 +17,7 @@ class UserInfoScreen extends StatefulWidget {
 }
 
 class _UserInfoScreenState extends State<UserInfoScreen> {
-  late bool _isEmailVerified;
   late User _user;
-
-  bool _verificationEmailBeingSent = false;
   bool _isSigningOut = false;
 
   Route _routeToSignInScreen() {
@@ -45,7 +42,6 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
   @override
   void initState() {
     _user = widget._user;
-    _isEmailVerified = _user.emailVerified;
 
     super.initState();
   }
@@ -93,133 +89,24 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
               ),
               SizedBox(height: 8.0),
               Text(
-                widget._user.displayName!,
+                _user.displayName!,
                 style: TextStyle(
                   color: CustomColors.firebaseYellow,
                   fontSize: 26,
                 ),
               ),
-              SizedBox(height: 24.0),
-              _isEmailVerified
-                  ? Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ClipOval(
-                          child: Material(
-                            color: Colors.greenAccent.withOpacity(0.6),
-                            child: Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Icon(
-                                Icons.check,
-                                size: 20,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 8.0),
-                        Text(
-                          'Email is verified',
-                          style: TextStyle(
-                            color: Colors.greenAccent,
-                            fontSize: 20,
-                            letterSpacing: 0.5,
-                          ),
-                        ),
-                      ],
-                    )
-                  : Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ClipOval(
-                          child: Material(
-                            color: Colors.redAccent.withOpacity(0.8),
-                            child: Padding(
-                              padding: const EdgeInsets.all(4.0),
-                              child: Icon(
-                                Icons.close,
-                                size: 20,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(width: 8.0),
-                        Text(
-                          'Email is not verified',
-                          style: TextStyle(
-                            color: Colors.redAccent,
-                            fontSize: 20,
-                            letterSpacing: 0.5,
-                          ),
-                        ),
-                      ],
-                    ),
               SizedBox(height: 8.0),
-              Visibility(
-                visible: !_isEmailVerified,
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    _verificationEmailBeingSent
-                        ? CircularProgressIndicator(
-                            valueColor: AlwaysStoppedAnimation<Color>(
-                              CustomColors.firebaseGrey,
-                            ),
-                          )
-                        : ElevatedButton(
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all(
-                                CustomColors.firebaseGrey,
-                              ),
-                              shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                            onPressed: () async {
-                              setState(() {
-                                _verificationEmailBeingSent = true;
-                              });
-                              await _user.sendEmailVerification();
-                              setState(() {
-                                _verificationEmailBeingSent = false;
-                              });
-                            },
-                            child: Padding(
-                              padding: EdgeInsets.only(top: 8.0, bottom: 8.0),
-                              child: Text(
-                                'Verify',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  color: CustomColors.firebaseNavy,
-                                  letterSpacing: 2,
-                                ),
-                              ),
-                            ),
-                          ),
-                    SizedBox(width: 16.0),
-                    IconButton(
-                      icon: Icon(Icons.refresh),
-                      onPressed: () async {
-                        User? user = await Authentication.refreshUser(_user);
-
-                        if (user != null) {
-                          setState(() {
-                            _user = user;
-                            _isEmailVerified = user.emailVerified;
-                          });
-                        }
-                      },
-                    ),
-                  ],
+              Text(
+                '( ${_user.email!} )',
+                style: TextStyle(
+                  color: CustomColors.firebaseOrange,
+                  fontSize: 20,
+                  letterSpacing: 0.5,
                 ),
               ),
               SizedBox(height: 24.0),
               Text(
-                'You are now signed in using Firebase Authentication. To sign out of your account click the "Sign Out" button below.',
+                'You are now signed in using your Google account. To sign out of your account click the "Sign Out" button below.',
                 style: TextStyle(
                     color: CustomColors.firebaseGrey.withOpacity(0.8),
                     fontSize: 14,
@@ -247,7 +134,7 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
                         setState(() {
                           _isSigningOut = true;
                         });
-                        await FirebaseAuth.instance.signOut();
+                        await Authentication.signOut(context: context);
                         setState(() {
                           _isSigningOut = false;
                         });
