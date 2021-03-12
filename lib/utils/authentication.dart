62e9f7791e71163bc39dd7b97ea03dfb2033dc2d
Souvik Biswas <sbis1999@gmail.com>
2021-03-12 14:10:18 +0530

    Add method for local auth

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterfire_samples/screens/user_info_screen.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:local_auth/local_auth.dart';
 
 class Authentication {
   static SnackBar customSnackBar({required String content}) {
@@ -115,5 +116,32 @@ class Authentication {
         ),
       );
     }
+  }
+
+  static Future<bool> authenticateWithBiometrics() async {
+    final LocalAuthentication localAuthentication = LocalAuthentication();
+    bool isBiometricSupported = await localAuthentication.isDeviceSupported();
+    bool canCheckBiometrics = await localAuthentication.canCheckBiometrics;
+
+    bool isAuthenticated = false;
+
+    if (isBiometricSupported && canCheckBiometrics) {
+      List<BiometricType> biometricTypes =
+          await localAuthentication.getAvailableBiometrics();
+      print(biometricTypes);
+
+      isAuthenticated = await localAuthentication.authenticate(
+        localizedReason: 'Please complete the biometrics to proceed.',
+        biometricOnly: true,
+        // androidAuthStrings: AndroidAuthMessages(
+        //   biometricHint: 'Verify identity using biometrics',
+        //   biometricRequiredTitle: 'Accessing secret vault',
+        //   deviceCredentialsRequiredTitle:
+        //       'Identity verification using biometrics is required to proceed to the secret vault.',
+        // ),
+      );
+    }
+
+    return isAuthenticated;
   }
 }
