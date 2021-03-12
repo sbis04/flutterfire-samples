40ec9758de11c67a45912b82859111b8a949cc71
Souvik Biswas <sbis1999@gmail.com>
2021-03-12 14:09:42 +0530

    Update Android files

--- android/app/src/main/kotlin/com/souvikbiswas/flutterfire_samples/MainActivity.kt
+++ android/app/src/main/kotlin/com/souvikbiswas/flutterfire_samples/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.souvikbiswas.flutterfire_samples
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
