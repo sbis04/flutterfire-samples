27ee55f1b971211f74a4bdbca93afc07b6a079d3
Souvik Biswas <sbis1999@gmail.com>
2021-03-18 17:29:45 +0530

    Add dashboard screen

---
+++ lib/screens/dashboard_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_samples/res/custom_colors.dart';
+import 'package:flutterfire_samples/screens/add_screen.dart';
+import 'package:flutterfire_samples/widgets/app_bar_title.dart';
+import 'package:flutterfire_samples/widgets/item_list.dart';
+
+class DashbaordScreen extends StatefulWidget {
+  @override
+  _DashbaordScreenState createState() => _DashbaordScreenState();
+}
+
+class _DashbaordScreenState extends State<DashbaordScreen> {
+  final FocusNode _nameFocusNode = FocusNode();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: CustomColors.firebaseNavy,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: CustomColors.firebaseNavy,
+        title: AppBarTitle(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => AddScreen(),
+            ),
+          );
+        },
+        backgroundColor: CustomColors.firebaseOrange,
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 32,
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: ItemList(),
+        ),
+      ),
+    );
+  }
+}
