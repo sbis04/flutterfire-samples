29bd81b54905c8ccb7325568715c902b9b6fac7b
Souvik Biswas <sbis1999@gmail.com>
2021-03-18 17:29:06 +0530

    Update main to include login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutterfire_samples/screens/sign_in_screen.dart';
+import 'package:flutterfire_samples/screens/login_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.indigo,
         brightness: Brightness.dark,
       ),
-      home: SignInScreen(),
+      home: LoginScreen(),
     );
   }
 }
