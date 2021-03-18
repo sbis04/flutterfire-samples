1f3f20c239653e7fe7a1de3f4843179b8a60ad59
Souvik Biswas <sbis1999@gmail.com>
2021-03-18 18:47:44 +0530

    Add item list

---
+++ lib/widgets/item_list.dart
@@ -0,0 +1,69 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutterfire_samples/res/custom_colors.dart';
+import 'package:flutterfire_samples/screens/edit_screen.dart';
+import 'package:flutterfire_samples/utils/database.dart';
+
+class ItemList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Database.readItems(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        } else if (snapshot.hasData || snapshot.data != null) {
+          return ListView.separated(
+            separatorBuilder: (context, index) => SizedBox(height: 16.0),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var noteInfo = snapshot.data!.docs[index].data()!;
+              String docID = snapshot.data!.docs[index].id;
+              String title = noteInfo['title'];
+              String description = noteInfo['description'];
+
+              return Ink(
+                decoration: BoxDecoration(
+                  color: CustomColors.firebaseGrey.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  onTap: () => Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => EditScreen(
+                        currentTitle: title,
+                        currentDescription: description,
+                        documentId: docID,
+                      ),
+                    ),
+                  ),
+                  title: Text(
+                    title,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  subtitle: Text(
+                    description,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              );
+            },
+          );
+        }
+
+        return Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(
+              CustomColors.firebaseOrange,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
