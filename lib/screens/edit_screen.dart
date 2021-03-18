9e7d80335844923651ceddffe534ffc32c5e38ae
Souvik Biswas <sbis1999@gmail.com>
2021-03-18 18:47:27 +0530

    Add edit screen

---
+++ lib/screens/edit_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_samples/res/custom_colors.dart';
+import 'package:flutterfire_samples/utils/database.dart';
+import 'package:flutterfire_samples/widgets/app_bar_title.dart';
+import 'package:flutterfire_samples/widgets/edit_item_form.dart';
+
+class EditScreen extends StatefulWidget {
+  final String currentTitle;
+  final String currentDescription;
+  final String documentId;
+
+  EditScreen({
+    required this.currentTitle,
+    required this.currentDescription,
+    required this.documentId,
+  });
+
+  @override
+  _EditScreenState createState() => _EditScreenState();
+}
+
+class _EditScreenState extends State<EditScreen> {
+  final FocusNode _titleFocusNode = FocusNode();
+
+  final FocusNode _descriptionFocusNode = FocusNode();
+
+  bool _isDeleting = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _titleFocusNode.unfocus();
+        _descriptionFocusNode.unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: CustomColors.firebaseNavy,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: CustomColors.firebaseNavy,
+          title: AppBarTitle(),
+          actions: [
+            _isDeleting
+                ? Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        CustomColors.firebaseOrange,
+                      ),
+                    ),
+                  )
+                : IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.redAccent,
+                      size: 32,
+                    ),
+                    onPressed: () async {
+                      setState(() {
+                        _isDeleting = true;
+                      });
+
+                      await Database.deleteItem(
+                        docId: widget.documentId,
+                      );
+
+                      setState(() {
+                        _isDeleting = false;
+                      });
+                    },
+                  ),
+          ],
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16.0,
+              right: 16.0,
+              bottom: 20.0,
+            ),
+            child: EditItemForm(
+              documentId: widget.documentId,
+              titleFocusNode: _titleFocusNode,
+              descriptionFocusNode: _descriptionFocusNode,
+              currentTitle: widget.currentTitle,
+              currentDescription: widget.currentDescription,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
