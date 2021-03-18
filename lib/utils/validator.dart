6cfa130709d5bdfb43186eccb8201d9e27bb5542
Souvik Biswas <sbis1999@gmail.com>
2021-03-18 17:30:24 +0530

    Add a validator for UID and the fields

--- lib/utils/validator.dart
+++ lib/utils/validator.dart
@@ -1,30 +1,17 @@
 class Validator {
-  static String? validateName({required String name}) {
-    if (name.isEmpty) {
-      return 'Name can\'t be empty';
+  static String? validateField({required String value}) {
+    if (value.isEmpty) {
+      return 'Field can\'t be empty';
     }
 
     return null;
   }
 
-  static String? validateEmail({required String email}) {
-    RegExp emailRegExp = RegExp(
-        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
-
-    if (email.isEmpty) {
-      return 'Email can\'t be empty';
-    } else if (!emailRegExp.hasMatch(email)) {
-      return 'Enter a correct email';
-    }
-
-    return null;
-  }
-
-  static String? validatePassword({required String password}) {
-    if (password.isEmpty) {
-      return 'Password can\'t be empty';
-    } else if (password.length < 6) {
-      return 'Enter a password with length at least 6';
+  static String? validateUserID({required String uid}) {
+    if (uid.isEmpty) {
+      return 'User ID can\'t be empty';
+    } else if (uid.length <= 3) {
+      return 'User ID should be greater than 3 characters';
     }
 
     return null;
