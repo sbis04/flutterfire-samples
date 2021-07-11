725f23729f36ee39638a2cf503731a6840ad237a
Souvik Biswas <sbis1999@gmail.com>
2021-07-11 17:45:05 +0530

    Add db screens for crud

---
+++ lib/screens/database/crud/db_add_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_samples/res/custom_colors.dart';
+import 'package:flutterfire_samples/widgets/app_bar_title.dart';
+import 'package:flutterfire_samples/widgets/database/crud/db_add_item_form.dart';
+
+class DbAddScreen extends StatelessWidget {
+  final FocusNode _titleFocusNode = FocusNode();
+  final FocusNode _descriptionFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _titleFocusNode.unfocus();
+        _descriptionFocusNode.unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: Palette.firebaseNavy,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Palette.firebaseNavy,
+          title: AppBarTitle(
+            sectionName: 'CRUD',
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16.0,
+              right: 16.0,
+              bottom: 20.0,
+            ),
+            child: DbAddItemForm(
+              titleFocusNode: _titleFocusNode,
+              descriptionFocusNode: _descriptionFocusNode,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/database/crud/db_edit_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutterfire_samples/res/custom_colors.dart';
+import 'package:flutterfire_samples/utils/database.dart';
+import 'package:flutterfire_samples/widgets/app_bar_title.dart';
+import 'package:flutterfire_samples/widgets/database/crud/db_edit_item_form.dart';
+
+class DbEditScreen extends StatefulWidget {
+  final String currentTitle;
+  final String currentDescription;
+  final String documentId;
+
+  DbEditScreen({
+    required this.currentTitle,
+    required this.currentDescription,
+    required this.documentId,
+  });
+
+  @override
+  _DbEditScreenState createState() => _DbEditScreenState();
+}
+
+class _DbEditScreenState extends State<DbEditScreen> {
+  final FocusNode _titleFocusNode = FocusNode();
+
+  final FocusNode _descriptionFocusNode = FocusNode();
+
+  bool _isDeleting = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _titleFocusNode.unfocus();
+        _descriptionFocusNode.unfocus();
+      },
+      child: Scaffold(
+        backgroundColor: Palette.firebaseNavy,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Palette.firebaseNavy,
+          title: AppBarTitle(sectionName: 'CRUD'),
+          actions: [
+            _isDeleting
+                ? Padding(
+                    padding: const EdgeInsets.only(
+                      top: 10.0,
+                      bottom: 10.0,
+                      right: 16.0,
+                    ),
+                    child: CircularProgressIndicator(
+                      valueColor: AlwaysStoppedAnimation<Color>(
+                        Colors.redAccent,
+                      ),
+                      strokeWidth: 3,
+                    ),
+                  )
+                : IconButton(
+                    icon: Icon(
+                      Icons.delete,
+                      color: Colors.redAccent,
+                      size: 32,
+                    ),
+                    onPressed: () async {
+                      setState(() {
+                        _isDeleting = true;
+                      });
+
+                      await Database.deleteItem(
+                        docId: widget.documentId,
+                      );
+
+                      setState(() {
+                        _isDeleting = false;
+                      });
+
+                      Navigator.of(context).pop();
+                    },
+                  ),
+          ],
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16.0,
+              right: 16.0,
+              bottom: 20.0,
+            ),
+            child: DbEditItemForm(
+              documentId: widget.documentId,
+              titleFocusNode: _titleFocusNode,
+              descriptionFocusNode: _descriptionFocusNode,
+              currentTitle: widget.currentTitle,
+              currentDescription: widget.currentDescription,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
