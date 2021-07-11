b0ce56214947a7fcce46f3bf3245b384116f5f26
Souvik Biswas <sbis1999@gmail.com>
2021-07-11 17:45:17 +0530

    Add db methods and validator

---
+++ lib/utils/database.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+final CollectionReference _mainCollection = _firestore.collection('notes');
+
+class Database {
+  static String? userUid;
+
+  static Future<void> addItem({
+    required String title,
+    required String description,
+  }) async {
+    DocumentReference documentReferencer =
+        _mainCollection.doc(userUid).collection('items').doc();
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "title": title,
+      "description": description,
+    };
+
+    await documentReferencer
+        .set(data)
+        .whenComplete(() => print("Note item added to the database"))
+        .catchError((e) => print(e));
+  }
+
+  static Future<void> updateItem({
+    required String title,
+    required String description,
+    required String docId,
+  }) async {
+    DocumentReference documentReferencer =
+        _mainCollection.doc(userUid).collection('items').doc(docId);
+
+    Map<String, dynamic> data = <String, dynamic>{
+      "title": title,
+      "description": description,
+    };
+
+    await documentReferencer
+        .update(data)
+        .whenComplete(() => print("Note item updated in the database"))
+        .catchError((e) => print(e));
+  }
+
+  static Stream<QuerySnapshot> readItems() {
+    CollectionReference notesItemCollection =
+        _mainCollection.doc(userUid).collection('items');
+
+    return notesItemCollection.snapshots();
+  }
+
+  static Future<void> deleteItem({
+    required String docId,
+  }) async {
+    DocumentReference documentReferencer =
+        _mainCollection.doc(userUid).collection('items').doc(docId);
+
+    await documentReferencer
+        .delete()
+        .whenComplete(() => print('Note item deleted from the database'))
+        .catchError((e) => print(e));
+  }
+}
---
+++ lib/utils/db_validator.dart
@@ -0,0 +1,19 @@
+class DbValidator {
+  static String? validateField({required String value}) {
+    if (value.isEmpty) {
+      return 'Field can\'t be empty';
+    }
+
+    return null;
+  }
+
+  static String? validateUserID({required String uid}) {
+    if (uid.isEmpty) {
+      return 'User ID can\'t be empty';
+    } else if (uid.length <= 3) {
+      return 'User ID should be greater than 3 characters';
+    }
+
+    return null;
+  }
+}
