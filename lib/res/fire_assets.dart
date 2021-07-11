3b62f1b6deba6b3f38fef11a02e9f0a6dd5c8b75
Souvik Biswas <sbis1999@gmail.com>
2021-07-11 13:38:35 +0530

    Add fire asset paths

---
+++ lib/res/fire_assets.dart
@@ -0,0 +1,9 @@
+class FireAssets {
+  static const String rootPath = 'assets';
+  static const String firebaseLogo = '$rootPath/firebase_logo.png';
+  static const String fireAuthentication = '$rootPath/authentication.png';
+  static const String fireDatabase = '$rootPath/database.png';
+  static const String fireBackend = '$rootPath/backend.png';
+  static const String fireMachineLearning = '$rootPath/machine-learning.png';
+  static const String fireOtherUtilities = '$rootPath/other-utilities.png';
+}
