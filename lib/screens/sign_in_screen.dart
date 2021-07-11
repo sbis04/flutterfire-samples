5a931b219d12cb99fe8dc9aa68bae5704dd60e98
Souvik Biswas <sbis1999@gmail.com>
2021-07-11 14:04:13 +0530

    Add back button to auth :smile:

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -42,71 +42,88 @@ class _SignInScreenState extends State<SignInScreen> {
       child: Scaffold(
         backgroundColor: Palette.firebaseNavy,
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.only(
-              left: 16.0,
-              right: 16.0,
-              bottom: 20.0,
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Row(),
-                Expanded(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Flexible(
-                        flex: 1,
-                        child: Image.asset(
-                          'assets/firebase_logo.png',
-                          height: 160,
-                        ),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 16.0,
+                  right: 16.0,
+                  bottom: 20.0,
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(),
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Flexible(
+                            flex: 1,
+                            child: Image.asset(
+                              'assets/firebase_logo.png',
+                              height: 160,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'FlutterFire',
+                            style: TextStyle(
+                              color: Palette.firebaseYellow,
+                              fontSize: 40,
+                            ),
+                          ),
+                          Text(
+                            'Authentication',
+                            style: TextStyle(
+                              color: Palette.firebaseOrange,
+                              fontSize: 40,
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(height: 20),
-                      Text(
-                        'FlutterFire',
-                        style: TextStyle(
-                          color: Palette.firebaseYellow,
-                          fontSize: 40,
-                        ),
-                      ),
-                      Text(
-                        'Authentication',
-                        style: TextStyle(
-                          color: Palette.firebaseOrange,
-                          fontSize: 40,
-                        ),
-                      ),
-                    ],
+                    ),
+                    FutureBuilder(
+                      future: _initializeFirebase(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return Text('Error initializing Firebase');
+                        } else if (snapshot.connectionState ==
+                            ConnectionState.done) {
+                          return SignInForm(
+                            emailFocusNode: _emailFocusNode,
+                            passwordFocusNode: _passwordFocusNode,
+                          );
+                        }
+                        return CircularProgressIndicator(
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            Palette.firebaseOrange,
+                          ),
+                        );
+                      },
+                    )
+                    // SignInForm(
+                    //   emailFocusNode: _emailFocusNode,
+                    //   passwordFocusNode: _passwordFocusNode,
+                    // ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black26,
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: BackButton(),
                   ),
                 ),
-                FutureBuilder(
-                  future: _initializeFirebase(),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) {
-                      return Text('Error initializing Firebase');
-                    } else if (snapshot.connectionState ==
-                        ConnectionState.done) {
-                      return SignInForm(
-                        emailFocusNode: _emailFocusNode,
-                        passwordFocusNode: _passwordFocusNode,
-                      );
-                    }
-                    return CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                        Palette.firebaseOrange,
-                      ),
-                    );
-                  },
-                )
-                // SignInForm(
-                //   emailFocusNode: _emailFocusNode,
-                //   passwordFocusNode: _passwordFocusNode,
-                // ),
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
